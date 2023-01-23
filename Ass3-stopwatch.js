ca2d8ed2aeda1a8abc21ab41a51f30fda5ee9617
abhijeet2279 <123387979+abhijeet2279@users.noreply.github.com>
2023-01-24 03:21:00 +0530

    Add files via upload

---
+++ Ass3-stopwatch.js
@@ -0,0 +1,20 @@
+
+let stopwatch = {
+    elapsedTime: 0,
+    intervalId: 0,
+    
+    start: function() {
+      if (this.intervalId) return;
+      this.intervalId = setInterval(() => {
+        this.elapsedTime += 1000;
+      }, 1000);
+    },
+    pause: function() {
+      clearInterval(this.intervalId);
+      this.intervalId = 0;
+    }, 
+    reset: function() {
+      this.pause();
+      this.elapsedTime = 0;
+    }
+  };
