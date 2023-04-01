2305c69a1f62696e647a17fc57247f5b6a0bcf6c
abhijeet2279 <123387979+abhijeet2279@users.noreply.github.com>
2023-04-01 09:02:22 +0530

    Create connection.js

---
+++ 9. Api Registration/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/student-reg').then(()=>{
+    console.log(`successfully connected`);
+    }).catch((e)=>{
+    console.log(`not connected`);
+    });
